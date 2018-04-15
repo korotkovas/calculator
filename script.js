8ffada161fd6006278a0ce60efffc9a145137435
korotkovas <31108046+korotkovas@users.noreply.github.com>
2018-04-15 19:51:07 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function plus () {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1); // parseint patikrins ar ivestas tik skaicius
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 + num2;
+	document.getElementById('res').innerHTML = result; //inner atsako uz isvedima mano rezultato kuri ivesiu
+
+}
+
+function minus () {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 - num2;
+	document.getElementById('res').innerHTML = result;
+
+}
+function multiply () {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 * num2;
+	document.getElementById('res').innerHTML = result;
+
+}
