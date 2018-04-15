9d4854d84d10bdf78dd283d5e1d90721338a3379
korotkovas <31108046+korotkovas@users.noreply.github.com>
2018-04-15 20:29:28 +0300

    #

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ function multiply () {
 	document.getElementById('res').innerHTML = result;
 
 }
+
+function devision () {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 / num2;
+	document.getElementById('res').innerHTML = result;
+
+}
